90c47aad4188d87e73e9551fe190f6943f23aee6
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-16 16:00:27 -0700

    created Book model, added insert

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Book = require('../lib/models/Book');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -10,5 +11,18 @@ describe('alchemy-app routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  test('creates a book', async () => {
+    const expected = {
+      title: 'The Sun Also Rises',
+      author: 'Ernest Hemingway',
+      pubish_date: 1926,
+      pages: 272,
+    };
+
+    const res = await request(app).get('/api/v1/books');
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
 });
