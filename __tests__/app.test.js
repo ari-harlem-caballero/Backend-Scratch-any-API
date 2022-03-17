0a90fbc531aa5bd9d686405b3a7e92252343e771
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-17 12:00:55 -0700

    added delete ability

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -62,4 +62,13 @@ describe('alchemy-app routes', () => {
     expect(res.body).toEqual(expected);
     expect(await Book.getBookById(book.id)).toEqual(expected);
   });
+
+  it('should delete single book based on ID', async () => {
+    const expected = await Book.getBookById(1);
+    
+    const res = await request(app)
+      .delete(`/api/v1/books/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
+  });
 });
